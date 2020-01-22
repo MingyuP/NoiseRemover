0215a922bf6ba3466f77438ea20ef82f3074d698
Mingyu Park <m52park@uwaterloo.ca>
2020-01-22 14:16:23 -0500

    Example Files added

--- NoiseRemove.py
+++ NoiseRemove.py
@@ -5,10 +5,11 @@ from numpy.fft import fft, ifft, fft2, ifft2, fftshift, ifftshift
 from scipy import ndimage, misc
 from scipy.signal import gaussian
 
-print('Enter File Name')
+print('Enter File Name without the extension')
 name = input()
-Omega, f = scipy.io.wavfile.read(name)
-Audio(f, rate=Omega)
+name_with_extension = name + '.wav'
+Omega, f = scipy.io.wavfile.read(name_with_extension)
+F = fft(f.T)
 
 # Some useful values
 N = len(f)    # total number of samples
@@ -17,7 +18,12 @@ t = np.arange(0,N) * L/N  # array of time stamps for samples
 
 omega = np.fft.fftshift(np.arange(-N/2, N/2)) / L
 
-tau = 2000
+tau = 1550
 mask = abs(omega) < tau
 Fe = F * mask
-Audio(ifft(Fe), rate=Omega)
+
+inverted_Fe = ifft(Fe)
+
+output_name = name + '_fixed.wav'
+scipy.io.wavfile.write(output_name, Omega, (inverted_Fe.astype(f.dtype).T))
+Audio(inverted_Fe, rate=Omega)
